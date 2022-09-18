8187d62a50ecbc4f392e9ba4d2334aa500ff21c4
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-18 16:29:31 -0300

    Sender connected to rabbitmq

--- src/Sender/Program.cs
+++ src/Sender/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Common.Messaging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostContext, configurationBuilder) =>
+    {
+        var env = hostContext.HostingEnvironment;
+        configurationBuilder
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables(prefix: "DOTNET_");
+    })
+    .ConfigureServices((hostContext, serviceCollection) =>
+    {
+        serviceCollection.AddRabbitMq(hostContext.Configuration);
+    })
+    .Build();
+
+host.Services.DeclareExchange(new ExchangeDefinition
+{
+    Name = "outbox_exchange",
+    Type = ExchangeType.Direct,
+    Durable = false,
+    AutoDelete = true
+});
+
+await host.RunAsync();
+    
