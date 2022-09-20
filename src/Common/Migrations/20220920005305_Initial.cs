787b37492788141e84b36dbefa21505e55451f7d
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-19 22:03:15 -0300

    context and migrations

---
+++ src/Common/Migrations/20220920005305_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Common.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventKey = table.Column<string>(type: "text", nullable: false),
+                    EventData = table.Column<string>(type: "text", nullable: false),
+                    EventDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SomeEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SomeEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxEvents");
+
+            migrationBuilder.DropTable(
+                name: "SomeEntities");
+        }
+    }
+}
---
+++ src/Common/Persistence/Context.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Common.Persistence;
+
+public sealed class Context : DbContext
+{
+    private readonly SenderSettings _senderSettings;
+
+    // ctor for migration generation
+    // public Context()
+    // {
+    //     _senderSettings = new SenderSettings
+    //     {
+    //         DatabaseConnectionString =
+    //             "Server=localhost;Port=5432;Database=OutboxPattern;User Id=admin;Password=senha_123;"
+    //     };
+    // }
+
+    public Context(IOptionsSnapshot<SenderSettings> senderSettings)
+    {
+        _senderSettings = senderSettings.Value;
+    }
+
+    public Context(SenderSettings senderSettings)
+    {
+        _senderSettings = senderSettings;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<SomeEntity>()
+            .Ignore(_ => _.BaseEvents);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(_senderSettings.DatabaseConnectionString);
+    }
+
+    public DbSet<SomeEntity> SomeEntities { get; set; }
+    public DbSet<OutboxEvent> OutboxEvents { get; set; }
+}
