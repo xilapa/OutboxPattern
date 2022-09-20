8333d833370e0c7b57ef090fcba1aac669ac66aa
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-19 22:10:04 -0300

    some entity domain event test

---
+++ tests/Domain.UnitTests/SomeEntityTests.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using Common.Persistence;
+using Domain.Entities;
+using Domain.Events;
+using Domain.Outbox;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.UnitTests;
+
+public sealed class SomeEntityTests
+{
+    [Fact]
+    public void SomeEntityCreatedEventIsCreatedCorrectly()
+    {
+        // Arrange and Act
+        var someEntity = new SomeEntity("test");
+        
+        // Assert
+        var @event = someEntity.BaseEvents.First(_ => _ is SomeEntityCreated);
+
+        (@event is SomeEntityCreated).Should().BeTrue();
+        
+        if(@event is SomeEntityCreated someEntityCreated)
+        {
+            someEntityCreated.CreatedEntityId.Should().Be(someEntity.Id);
+            someEntityCreated.CreatedEntityName.Should().Be(someEntity.Name);
+        }
+    }
+}
