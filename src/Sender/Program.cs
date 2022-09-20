59152cab44568240691e863c9502893b016d99c5
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-20 19:04:56 -0300

    using the empty host builder as starting point

--- src/Sender/Program.cs
+++ src/Sender/Program.cs
@@ -4,20 +4,28 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var host = Host.CreateDefaultBuilder(args)
+var host = new HostBuilder()
+    .ConfigureDefaults(args)
+    .ConfigureHostConfiguration(configurationBuilder =>
+        configurationBuilder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables(prefix: "DOTNET_")
+            .AddCommandLine(args)
+    )
     .ConfigureAppConfiguration((hostContext, configurationBuilder) =>
     {
         var env = hostContext.HostingEnvironment;
         configurationBuilder
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables(prefix: "DOTNET_");
+            .AddEnvironmentVariables(prefix: "DOTNET_")
+            .AddCommandLine(args);
     })
     .ConfigureServices((hostContext, serviceCollection) =>
-    {
-        serviceCollection.AddRabbitMq(hostContext.Configuration);
-        serviceCollection.Configure<SenderSettings>(hostContext.Configuration.GetSection(nameof(SenderSettings)));
-    })
+        serviceCollection
+            .AddRabbitMq(hostContext.Configuration)
+            .Configure<SenderSettings>(hostContext.Configuration.GetSection(nameof(SenderSettings)))
+    )
     .Build();
 
 host.Services.DeclareExchange(new ExchangeDefinition
