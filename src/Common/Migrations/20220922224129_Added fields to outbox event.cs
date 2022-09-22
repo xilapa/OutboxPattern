15df5df04da1f7f6edabfb0dd556f6e873e9a1e5
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-22 19:42:42 -0300

    fields added to outbox event

---
+++ src/Common/Migrations/20220922224129_Added fields to outbox event.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Common.Migrations
+{
+    public partial class Addedfieldstooutboxevent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "OutboxEvents",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRetryDate",
+                table: "OutboxEvents",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishingDate",
+                table: "OutboxEvents",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Retries",
+                table: "OutboxEvents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "OutboxEvents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "OutboxEvents");
+
+            migrationBuilder.DropColumn(
+                name: "LastRetryDate",
+                table: "OutboxEvents");
+
+            migrationBuilder.DropColumn(
+                name: "PublishingDate",
+                table: "OutboxEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Retries",
+                table: "OutboxEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "OutboxEvents");
+        }
+    }
+}
--- src/Common/Outbox/OutboxEvent.cs
+++ src/Common/Outbox/OutboxEvent.cs
@@ -1,24 +1,58 @@
 ﻿using System.Text.Json;
-using Domain.Events;
 
 namespace Common.Outbox;
 
 public sealed class OutboxEvent
 {
-    // for EF Core
+    // Ctor for EF Core
+    // ReSharper disable once UnusedMember.Local
     private OutboxEvent()
     { }
 
-    public OutboxEvent(DomainEvent @event)
+    public OutboxEvent(object @event)
     {
         Id = Guid.NewGuid();
-        EventDate = DateTime.UtcNow;
         EventKey = @event.GetType().Name;
-        EventData = JsonSerializer.Serialize(@event as object);
+        EventData = JsonSerializer.Serialize(@event);
+        EventDate = DateTime.UtcNow;
+        PublishingDate = null;
+        LastRetryDate = null;
+        Status = PublishingStatus.NotPublished;
+        Retries = 0;
+        CurrentRetries = 0;
+        ExpirationDate = null;
     }
 
-    public Guid Id { get; protected set; }
-    public string EventKey { get; protected set; }
-    public string EventData { get; protected set; }
-    public DateTime EventDate { get; protected set; }
+    public Guid Id { get; private set; }
+    public string EventKey { get; private set; }
+    public string EventData { get; private set; }
+    public DateTime EventDate { get; private set; }
+    public DateTime? PublishingDate { get; private set; }
+    public DateTime? LastRetryDate { get; private set; }
+    public PublishingStatus Status { get; private set; }
+    public int Retries { get; private set; }
+    public int CurrentRetries { get; private set; }
+    public DateTime? ExpirationDate { get; set; }
+
+    public void SetPublishedStatus()
+    {
+        Status = PublishingStatus.Published;
+    }
+
+    public void SetErrOnPublishStatus()
+    {
+        Status = PublishingStatus.ErrorOnPublish;
+    }
+
+    public void IncrementRetryCount()
+    {
+        CurrentRetries++;
+    }
+}
+
+public enum PublishingStatus
+{
+    NotPublished = 1,
+    Published,
+    ErrorOnPublish
 }
