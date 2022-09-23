a49c0058b676daf2732bb39d812f2d35459bc7fd
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-23 17:35:25 -0300

    BaseQueue: logs and method to avoid enqueue itens if the queue is under heavy load

--- src/Common/Outbox/Base/BaseQueue.cs
+++ src/Common/Outbox/Base/BaseQueue.cs
@@ -1,23 +1,45 @@
 ﻿using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
 
 namespace Common.Outbox.Base;
 
 public abstract class BaseQueue : IBaseQueue
 {
+    private readonly int _capacity;
+    private readonly ILogger _logger;
     private readonly Channel<OutboxEvent> _queue;
 
-    protected BaseQueue(int capacity)
+    protected BaseQueue(int capacity, ILogger logger, BoundedChannelFullMode channelMode = BoundedChannelFullMode.Wait)
     {
+        _capacity = capacity;
+        _logger = logger;
         var boundedChannelOptions = new BoundedChannelOptions(capacity)
         {
             SingleReader = true,
-            FullMode = BoundedChannelFullMode.Wait
+            FullMode = channelMode
         };
         _queue = Channel.CreateBounded<OutboxEvent>(boundedChannelOptions);
     }
 
-    public async ValueTask Enqueue(OutboxEvent @event, CancellationToken cancellationToken) =>
+    public async ValueTask Enqueue(OutboxEvent @event, CancellationToken cancellationToken)
+    {
+        if(_queue.Reader.Count > _capacity * 0.9)
+            _logger.LogWarning("{CurrentTime} Queue is under heavy load, current count: {Count}",DateTime.UtcNow, _queue.Reader.Count);
         await _queue.Writer.WriteAsync(@event, cancellationToken);
+    }
+
+    public async ValueTask<bool> TryEnqueue(OutboxEvent @event, CancellationToken cancellationToken)
+    {
+        var currentCount = _queue.Reader.Count;
+        // TODO: make this available capacity window configurable
+        if (currentCount > _capacity - 1_000)
+        {
+            _logger.LogWarning("{CurrentTime} Event not enqueued, queue is under heavy load, current count: {Count}",DateTime.UtcNow, _queue.Reader.Count);
+            return false;
+        }
+        await _queue.Writer.WriteAsync(@event, cancellationToken);
+        return true;
+    }
 
     public async ValueTask<OutboxEvent> Dequeue(CancellationToken cancellationToken) =>
         await _queue.Reader.ReadAsync(cancellationToken);
@@ -29,6 +51,15 @@ public abstract class BaseQueue : IBaseQueue
 public interface IBaseQueueWriter
 {
     ValueTask Enqueue(OutboxEvent @event, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// If the remaining slots on the queue is lower than 1_000 elements, do not enqueue and return false.
+    /// Otherwise, returns true.
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<bool> TryEnqueue(OutboxEvent @event, CancellationToken cancellationToken);
 }
 
 public interface IBaseQueueReader
