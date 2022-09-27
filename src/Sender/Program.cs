ecde4f91e5e5f99050dd496c4beddf99adcc15b6
Dirceu Junior <dirceu.sjr@gmail.com>
2022-09-27 13:09:16 -0300

    connecting everything on program

--- src/Sender/Program.cs
+++ src/Sender/Program.cs
@@ -1,8 +1,17 @@
 ﻿using Common;
 using Common.Messaging;
+using Common.Outbox;
+using Common.Outbox.EventPublisher;
+using Common.Outbox.EventRetry;
+using Common.Outbox.EventReviver;
+using Common.Outbox.EventSaver;
+using Common.Persistence;
+using Domain.Events;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Sender;
 
 var host = new HostBuilder()
     .ConfigureDefaults(args)
@@ -25,16 +34,37 @@ var host = new HostBuilder()
         serviceCollection
             .AddRabbitMq(hostContext.Configuration)
             .Configure<SenderSettings>(hostContext.Configuration.GetSection(nameof(SenderSettings)))
+            .AddDbContext<Context>()
+            .AddSingleton<OutboxEventQueue>()
+            .AddSingleton<IOutboxEventQueueWriter>(sp => sp.GetRequiredService<OutboxEventQueue>())
+            .AddSingleton<IOutboxEventQueueReader>(sp => sp.GetRequiredService<OutboxEventQueue>())
+            .AddSingleton<IOutboxEventRetryQueue, OutboxEventRetryQueue>()
+            .AddHostedService<OutboxEventRetryPublisher>()
+            .AddHostedService<OutboxEventPublisher>()
+            .AddSingleton<IOutboxEventSaveQueue, OutboxEventSaveQueue>()
+            .AddHostedService<EventSaverService>()
+            .AddSingleton<IDatabaseConnection, DatabaseConnection>()
+            .AddHostedService<OutboxEventReviver>()
+            .AddHostedService<WorkGenerator>()
     )
     .Build();
 
-host.Services.DeclareExchange(new ExchangeDefinition
-{
-    Name = "outbox_exchange",
-    Type = ExchangeType.Direct,
-    Durable = false,
-    AutoDelete = true
-});
+var senderSettings = host.Services.GetRequiredService<IOptions<SenderSettings>>().Value;
+
+host.Services
+    .DeclareExchange(new ExchangeDefinition
+    {
+        Name = senderSettings.Exchange,
+        Type = ExchangeType.Direct,
+        Durable = true,
+        AutoDelete = false
+    })
+    .DeclareQueueAndBind(typeof(SomeEntityCreated),
+        new QueueDefinition
+        {
+            Durable = false,
+            AutoDelete = false,
+            ExchangeToBind = senderSettings.Exchange
+        });
 
 await host.RunAsync();
-    
